19ea8b5bb2be41b63acba4c1d5b27efc550376d5
Luiz Gustavo L Moura <lglmoura@gmail.com>
2023-08-03 18:27:01 -0300

    test: add instituto with mongodb

---
+++ tests/infra/db/mongodb/instituto-mongo-repository.spec.ts
@@ -0,0 +1,34 @@
+import { InstitutoMongoRepository, MongoHelper } from '@/infra/db'
+import { mockAddInstitutoParams } from '@/tests/domain/mocks'
+
+import { Collection } from 'mongodb'
+
+let institutoCollection: Collection
+
+const makeSut = (): InstitutoMongoRepository => {
+  return new InstitutoMongoRepository()
+}
+
+describe('InstitutoMongoRepository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    institutoCollection = MongoHelper.getCollection('accounts')
+    await institutoCollection.deleteMany({})
+  })
+  describe('add()', () => {
+    test('Should return an account on success', async () => {
+      const sut = makeSut()
+      const addInstitutoParams = mockAddInstitutoParams()
+      const isValid = await sut.add(addInstitutoParams)
+      expect(isValid).toBe(true)
+    })
+  })
+})
+
