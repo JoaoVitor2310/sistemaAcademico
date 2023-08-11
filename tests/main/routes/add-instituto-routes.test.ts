f52db2cef9de2e379a7e41f5af800027e6557956
Luiz Gustavo L Moura <lglmoura@gmail.com>
2023-08-11 21:22:22 +0000

    refactor: Add mongodg in intregatrion test

--- tests/main/routes/add-instituto-routes.test.ts
+++ tests/main/routes/add-instituto-routes.test.ts
@@ -1,12 +1,28 @@
+
+import { MongoHelper } from '@/infra/db'
 import { setupApp } from '@/main/config/app'
 
 import { Express } from 'express'
 import request from 'supertest'
+import { Collection } from 'mongodb'
+
+let institutoCollection: Collection
+
 let app: Express
 
 describe('Instituto Routes', () => {
   beforeAll(async () => {
     app = setupApp()
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    institutoCollection = MongoHelper.getCollection('instituto')
+    await institutoCollection.deleteMany({})
   })
 
   test('Should return an instituto on sucess', async () => {
