34d606a1cb35dffde1895f00af266308ea1197dd
Luiz Gustavo L Moura <lglmoura@gmail.com>
2023-08-15 20:41:17 +0000

    refactor: test instituto controller

--- tests/presentation/controllers/instituto-controller.spec.ts
+++ tests/presentation/controllers/instituto-controller.spec.ts
@@ -1,5 +1,5 @@
 import { AddInstitutoController } from '@/presentation/controllers'
-// import { badRequest, serverError, noContent } from '@/presentation/helpers'
+import { badRequest, noContent } from '@/presentation/helpers'
 import { ValidationSpy, AddInstitutoSpy } from '@/tests/presentation/mocks'
 // import { throwError } from '@/tests/domain/mocks'
 import { faker } from '@faker-js/faker'
@@ -30,10 +30,23 @@ const makeSut = (): SutTypes => {
 }
 
 describe('Insttuto Controller', () => {
-  test('Should return 400 if Validation Na', async () => {
+  test('Should call Validation with correct values', async () => {
     const { sut, validationSpy } = makeSut()
     const request = mockRequest()
     await sut.handle(request)
     expect(validationSpy.input).toEqual(request)
+  })
+
+  test('Should return 400 if Validation fails', async () => {
+    const { sut, validationSpy } = makeSut()
+    validationSpy.error = new Error()
+    const httpResponse = await sut.handle(mockRequest())
+    expect(httpResponse).toEqual(badRequest(validationSpy.error))
+  })
+
+  test('Should return 204 on success', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle(mockRequest())
+    expect(httpResponse).toEqual(noContent())
   })
 })
