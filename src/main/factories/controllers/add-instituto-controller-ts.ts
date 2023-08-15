100b40b14b2a9ebf15f9eddf2304f6da3e6828e6
Luiz Gustavo L Moura <lglmoura@gmail.com>
2023-08-15 20:36:59 +0000

    refactor: rename add instituto controller

--- src/main/factories/controllers/add-instituto-controller-ts.ts
+++
@@ -1,9 +0,0 @@
-import { AddInstitutoController } from '@/presentation/controllers'
-import { makeAddInstitutoValidation } from './add-instituto-validation-factory'
-import { makeDbAddInstituto } from '../usecases/add-instituto-factory'
-
-export const makeAddInstitutoController = (): AddInstitutoController => {
-  const addInstitutoController = new AddInstitutoController(makeAddInstitutoValidation(),makeDbAddInstituto())
-
-  return addInstitutoController
-}
